fc024931a7501fe344858d4c3ee1d38ac959cf82
Ilya Chernyaev <ilyachernyaevdev@gmail.com>
2021-02-06 03:48:15 +0300

    add removeElements function

--- js/script.js
+++ js/script.js
@@ -9,4 +9,12 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
+const commercialBlock = document.querySelector('.promo__adv');
+const banners = commercialBlock.querySelectorAll('img');
+
+function removeElements([...elements]) {
+    elements.forEach(element => element.remove());
+}
+
+removeElements(banners);
 
