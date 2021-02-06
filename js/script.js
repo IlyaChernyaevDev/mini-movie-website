50b9e4cd9752cd0826ee56b437a8b2802e21f53c
Ilya Chernyaev <ilyachernyaevdev@gmail.com>
2021-02-06 04:07:34 +0300

    sort movies array and add films in movie list

--- js/script.js
+++ js/script.js
@@ -14,9 +14,15 @@ const promoBG = document.querySelector('.promo__bg');
 const commercialBlock = document.querySelector('.promo__adv');
 const banners = commercialBlock.querySelectorAll('img');
 const genre = promoBG.querySelector('.promo__genre');
+const filmsArray = document.querySelectorAll('.promo__interactive-item');
 
 promoBG.style.background = `url('img/bg.jpg') center center/cover no-repeat`;
 genre.textContent = 'ДРАМА';
+
+movieDB.movies.sort();
+filmsArray.forEach((item, index) => {
+    item.textContent = `${++index}. ${movieDB.movies[--index]}`;
+});
 
 function removeElements([...elements]) {
     elements.forEach(element => element.remove());
