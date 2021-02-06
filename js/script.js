6c49a5d0b897301279c8a8c396510444cb249739
Ilya Chernyaev <ilyachernyaevdev@gmail.com>
2021-02-06 03:58:31 +0300

    changed the background of the promo__bg block

--- js/script.js
+++ js/script.js
@@ -9,8 +9,14 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
+
+const promoBG = document.querySelector('.promo__bg');
 const commercialBlock = document.querySelector('.promo__adv');
 const banners = commercialBlock.querySelectorAll('img');
+const genre = promoBG.querySelector('.promo__genre');
+
+promoBG.style.background = `url('img/bg.jpg') center center/cover no-repeat`;
+genre.textContent = 'ДРАМА';
 
 function removeElements([...elements]) {
     elements.forEach(element => element.remove());
